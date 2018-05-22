7359f9f81dcd235048a7dc1d487a0639fafd60bc
Bwukki <bwukki@gmail.com>
2018-05-22 00:19:10 -0700

    Fixed partytime

--- AbyssBot/Program.cs
+++ AbyssBot/Program.cs
@@ -54,9 +54,9 @@ namespace MyBot
             }
             if (message.Content == "!partytime")
             {
-                await message.Author.SendMessageAsync("<:TestMsg4:448379297405730827><:TestMsg5:448379298714353674><:TestMsg6:448379299666591744><:TestMsg2:448379158817538048><:TestMsg3:448379160553848842><:TestMsg1:448379158620536832><:TestMsg3:448379160553848842><:TestMsg2:448379158817538048><:TestMsg6:448379299666591744><:TestMsg5:448379298714353674><:TestMsg4:448379297405730827>");
-                await message.Author.SendMessageAsync("<:SpriteServerTest1:448374300135915542> <:SpriteServerTest2:448374300135915542> PARTY TIME <:SpriteServerTest2:448374300135915542> <:SpriteServerTest1:448374300135915542>");
-                await message.Author.SendMessageAsync("<:Test8:448380788019298324> <:Test9:448380871171637260> <:Test7:448380787075842051> https://gifsound.com/?gifv=5xa3bCB&v=NvS351QKFV4&s=70 <:Test7:448380787075842051> <:Test9:448380871171637260><:Test8:448380788019298324>");
+                await message.Author.SendMessageAsync("<a:TestMsg4:448379297405730827><a:TestMsg5:448379298714353674><a:TestMsg6:448379299666591744><a:TestMsg2:448379158817538048><a:TestMsg3:448379160553848842><a:TestMsg1:448379158620536832><a:TestMsg3:448379160553848842><a:TestMsg2:448379158817538048><a:TestMsg6:448379299666591744><a:TestMsg5:448379298714353674><a:TestMsg4:448379297405730827>");
+                await message.Author.SendMessageAsync(" <a:SpriteServerTest1:448373306207240192> <a:SpriteServerTest2:448374244079042561> PARTY TIME <a:SpriteServerTest2:448374244079042561> <a:SpriteServerTest1:448373306207240192>");
+                await message.Author.SendMessageAsync("<a:Test8:448380788019298324> <a:Test9:448380871171637260> <a:Test7:448380787075842051> https://gifsound.com/?gifv=5xa3bCB&v=NvS351QKFV4&s=70 <a:Test7:448380787075842051> <a:Test9:448380871171637260><a:Test8:448380788019298324>");
             }
         }
 
