497965d7c09b2df1c8a601acdb8b870007876d14
Bwukki <bwukki@gmail.com>
2018-05-21 22:49:36 -0700

    Added secret shit

--- AbyssBot/Program.cs
+++ AbyssBot/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
-namespace AbyssBot
+namespace MyBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            DiscordSocketClient client = new DiscordSocketClient();
+            client.Log += Log;
+
+            string token = "blahblahblah";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1); //stops the program from exiting main until the program is closed
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
+
