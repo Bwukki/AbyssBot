5a7ad929386efead45b44a0559b3118c8722b07f
Kowz <ko_eh@hotmail.com>
2018-05-22 00:08:34 -0700

    Bot Multi-PM Test
    Testing bot capability of multiple simultaneous PMs.

--- AbyssBot/Program.cs
+++ AbyssBot/Program.cs
@@ -52,6 +52,12 @@ namespace MyBot
             {
                 await message.Author.SendMessageAsync("This is a test, oni-chan!");
             }
+            if (message.Content == "!partytime")
+            {
+                await message.Author.SendMessageAsync("<:TestMsg4:448379297405730827><:TestMsg5:448379298714353674><:TestMsg6:448379299666591744><:TestMsg2:448379158817538048><:TestMsg3:448379160553848842><:TestMsg1:448379158620536832><:TestMsg3:448379160553848842><:TestMsg2:448379158817538048><:TestMsg6:448379299666591744><:TestMsg5:448379298714353674><:TestMsg4:448379297405730827>");
+                await message.Author.SendMessageAsync("<:SpriteServerTest1:448374300135915542> <:SpriteServerTest2:448374300135915542> PARTY TIME <:SpriteServerTest2:448374300135915542> <:SpriteServerTest1:448374300135915542>");
+                await message.Author.SendMessageAsync("<:Test8:448380788019298324> <:Test9:448380871171637260> <:Test7:448380787075842051> https://gifsound.com/?gifv=5xa3bCB&v=NvS351QKFV4&s=70 <:Test7:448380787075842051> <:Test9:448380871171637260><:Test8:448380788019298324>");
+            }
         }
 
     }
